4f697ff80cb13ec6c78fc00735476840e2c41ccb
Hasan <hasan.asi581@gmail.com>
2021-05-03 09:34:20 +0300

    mmmmdfsfddasdasd

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("shduaishdasd");
+        System.out.println("shduaishdasd");
         // some changes
     }
-aa
 }
